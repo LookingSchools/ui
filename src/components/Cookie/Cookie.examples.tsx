b650cfcb5e6c3783784961dadb109ecb4c48a5df
Sergey Bocharov <sergei-b84@bk.ru>
2020-11-05 20:36:01 +0300

    Added i18n

--- src/components/Cookie/Cookie.examples.tsx
+++ src/components/Cookie/Cookie.examples.tsx
@@ -3,6 +3,9 @@ import { storiesOf } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs";
 import { withDocs } from "@storybook-addons/docs";
 
+import i18n, { setI18nLang } from '@lookingschools/i18n';
+import * as keyset from './Cookie.i18n';
+
 import { Cookie } from "./Cookie.bundle";
 
 storiesOf("LookingSchools/Components|Cookie", module)
@@ -14,11 +17,16 @@ storiesOf("LookingSchools/Components|Cookie", module)
       }
     })
   )
-  .add("preview", () => (
-    <div>
+  .add("preview", () => {
+   
+    setI18nLang('en');
+    
+    const exampleI18N = i18n(keyset);
+
+    return (<div>
       <Cookie
-        text="Мы cохраняем файлы cookie: это помогает сайту работать лучше."
+        text={exampleI18N('Мы cохраняем файлы cookie: это помогает сайту работать лучше.')}
         button="Хорошо"
       />
-    </div>
-  ));
+    </div>)
+  });
---
+++ src/components/Cookie/Cookie.i18n/en.ts
@@ -0,0 +1,3 @@
+export const en = {
+    "Мы cохраняем файлы cookie: это помогает сайту работать лучше.": "We save cookies. It helps us provide you with the best experience we can.",
+};
